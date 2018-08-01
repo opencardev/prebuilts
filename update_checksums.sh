3ee1e488a34ddbd5d46f4e8878105b2c43078105
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-08-01 16:35:20 +0200

    Switch updates to md5 check

---
+++ update_checksums.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd csmt
+md5sum $(basename crankshaft) > crankshaft.md5
+cd ..
+
+cd gpio2kbd
+md5sum $(basename gpio2kbd) > gpio2kbd.md5
+cd ..
+
+cd openauto
+md5sum $(basename autoapp) > autoapp.md5
+md5sum $(basename autoapp_helper) > autoapp_helper.md5
+md5sum $(basename btservice) > btservice.md5
+md5sum $(basename libaasdk.so) > libaasdk.so.md5
+md5sum $(basename libaasdk_proto.so) > libaasdk_proto.so.md5
+cd ..
+
+cd udev
+md5sum $(basename 51-android.rules) > 51-android.rules.md5
+cd ..
